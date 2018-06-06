943ef78964bd91800f42095c764758bad6beef6d
mbranting <mckenna.branting@gmail.com>
2018-06-06 22:22:43 +0000

    automatically backed up by learn

--- cli_notes.rb
+++ cli_notes.rb
@@ -1,1 +1,11 @@
-puts "What's your favorite animal?"
+# puts "What's your favorite animal?"
+# answer = gets.chomp
+# #only gets takes in input from the terminal with the new line character
+# #gets.chomp takes in input from the terminal
+ 
+puts "Please input your first name:"
+first_name = gets.chomp
+puts "Please input your last name:"
+last_name = gets.chomp
+game_number = "75"
+puts "Welcome to the #{game number}th Hunger Games, #{first_name} #{last_name}."

2a9dd6f549dd118a67894b0972350d8e52046eea
mbranting <mckenna.branting@gmail.com>
2018-06-06 22:28:57 +0000

    automatically backed up by learn

--- cli_notes.rb
+++ cli_notes.rb
@@ -3,12 +3,12 @@
 # #only gets takes in input from the terminal with the new line character
 # #gets.chomp takes in input from the terminal
  
-puts "Please input your first name:"
-first_name = gets.chomp
-puts "Please input your last name:"
-last_name = gets.chomp
-game_number = "75"
-puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name}."
+# puts "Please input your first name:"
+# first_name = gets.chomp
+# puts "Please input your last name:"
+# last_name = gets.chomp
+# game_number = "75"
+# puts "Welcome to the #{game_number}th Hunger Games, #{first_name} #{last_name}."
 
 #Make a program that takes in a single integer value, and prints 'odd' or 'even' depending on which it is. The program should run forever, always waiting for input after it processes the previous
 
